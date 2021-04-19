166189bb17bf8229c1ec5773091b9c6bfa170813
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-04-19 09:05:04 -0300

    Añade ejecutable, corrige error.

--- temperature conversion/main.c
+++ temperature conversion/main.c
@@ -20,15 +20,13 @@ int main (void) {
         printf("Ingrese por favor el valor en grados celsius: "); scanf("%f", &celsius);
 
         fahrenheit = (celsius * 9/5) + 32;
-        printf("El valor en grados fahrenheit para, los %.1f grados celsius ingresados es:
-                %.1f \n", celsius, fahrenheit);
+        printf("El valor en grados fahrenheit para, los %.1f grados celsius ingresados es: %.1f \n", celsius, fahrenheit);
 
     }else if (decisionDeUsuario == 2) {
         printf("Ingrse por favor el valor en grados fahrenheit: "); scanf("%f", &fahrenheit);
 
         celsius = ((fahrenheit - 32) * 5) / 9;
-        printf("El valor en grados celsius para, los %.1f grados fahrenheit ingresados es: %.1f \n"
-               , fahrenheit, celsius);
+        printf("El valor en grados celsius para, los %.1f grados fahrenheit ingresados es: %.1f \n", fahrenheit, celsius);
     } else {
         printf("Es posible que usted haya ingresado un valor incorrecto \n");
     }
