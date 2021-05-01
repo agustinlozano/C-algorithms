4e0af3100c9eb3eb4156b5cb2d32796470fd15ff
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-05-01 12:10:11 -0300

    Cambios en Star Wars Program Se añade una funcion nueva, para atomizar aun mas la funcion main, ademas se realizan algunos cambios que pretenden agregar legibilidad.
    Aun así, quedan cuestiones que generan cosas inesperadas en el
    programa.

--- Star Wars Program/main.c
+++ Star Wars Program/main.c
@@ -1,183 +1,35 @@
-/* Un programa que muestre un menu
- * donde el usuario pueda elegir
- * el episodio de una pelicula del
- * universo de Star Wars, y el pro-
- * grama muestre el nombre corres-
- * pondiente.
+/* Un programa con un menu donde el usuario pueda elegir
+ * el episodio de una pelicula del universo de Star Wars,
+ * y programa muestre el nombre correspondiente, mas un
+ * poco de informacion.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define PRIMERA_OPCION 1
-#define ULTIMA_OPCION 10
-#define Si 'y'
-#define No 'n'
+#define ULTIMA_OPCION 9
+
+#define SI 'y'
+#define NO 'n'
 
 // Prototipado
-int menu();
+int evaluarMenu();
 void regresarAlMenu();
+void mostrarContenidoSegun(int decisionUsusario);
 
 // Main
 int main(void) {
   int decisionUsusario;
-  char decisionUsusario2;
-  
-  decisionUsusario = menu();
+ 
+  decisionUsusario = evaluarMenu();
+  mostrarContenidoSegun(decisionUsusario);
 
-  if (decisionUsusario >= PRIMERA_OPCION && decisionUsusario < ULTIMA_OPCION) {
-    switch(decisionUsusario) {
-      case 1: printf("\nStar Wars: Episode I - The Phantom Menace\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 1999,\n");	
-		printf("ademas, llego a recaudar 1027 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-      break;
-
-      case 2: printf("\nStar Wars: Episode II – Attack of the Clones\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 2002,\n");	
-		printf("ademas, llego a recaudar 649 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();	
-	      }
-
-      break;
-
-      case 3: printf("\nStar Wars: Episode III – Revenge of the Sith\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 2005,\n");	
-		printf("ademas, llego a recaudar 848 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-	        regresarAlMenu();
-	      }
-      break;
-
-      case 4: printf("\nStar Wars: Episode IV A New Hope\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 1977,\n");	
-		printf("ademas, llego a recaudar 775,8 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-      break;
-
-      case 5: printf("\nStar Wars: Episode V - The Empire Strikes Back\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 1980,\n");	
-		printf("ademas, llego a recaudar 550,9 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-      break;
-
-      case 6: printf("\nStar Wars: Episode VI - Return of the Jedi\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 1983,\n");	
-		printf("ademas, llego a recaudar 475 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-      break;
-
-      case 7: printf("\nStar Wars: Episode VII – The Force Awakens\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 2015,\n");	
-		printf("ademas, llego a recaudar 2066 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-
-      break;
-
-      case 8: printf("\nStar Wars: Episode VIII - The Last Jedi\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 2017,\n");	
-		printf("ademas, llego a recaudar 1333 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-
-      break;
-
-      case 9: printf("\nStar Wars: Episode IX: The Rise of Skywalker\n");
-	      printf("\nDesea usted, saber mas sobre la pelicula? [y/n] ");
-	      scanf(" %c", &decisionUsusario2);
-	      
-	      if (decisionUsusario2 == Si || decisionUsusario2 == No) {
-		printf("\nLa pelicula se estreno mundialmente en 2019,\n");	
-		printf("ademas, llego a recaudar 1074 millones de dolares.\n");
-		regresarAlMenu();
-
-	      } else {
-		printf("Error: Usted ha ingresado una opcion invalida.");	
-		regresarAlMenu();
-	      }
-
-      break;
-
-    }
-
-  } else {
-    printf("\nError: Usted ha ingresado una opcion invalida.\n");
-    regresarAlMenu();
-  }
-
-  
   return EXIT_SUCCESS;
 }
 
-int menu() {
+// Definicion de funciones
+int evaluarMenu() {
 
   int respuesta;
   printf("\tEl universo de Star Wars y sus peliculas \n");
@@ -205,10 +57,194 @@ void regresarAlMenu() {
   printf("\nDesea volver al menu principal? [y/n] ");
   scanf(" %c", &decisionUsusario3);
 
-  if (decisionUsusario3 == Si) {
+  if (decisionUsusario3 == SI) {
     main();
-  } else {
+  } else if (decisionUsusario3 == NO) {
     EXIT_SUCCESS;
+  } else {
+    printf("ERROR: usted ingreso una opcion invalida!\n");
+    EXIT_SUCCESS;
+  }
+}
+
+void mostrarContenidoSegun(int decisionUsusario) {
+  char decisionUsusario2;
+
+  if (decisionUsusario >= PRIMERA_OPCION && decisionUsusario <= ULTIMA_OPCION) {
+    switch(decisionUsusario) {
+      case 1: printf("\nStar Wars: Episode I - The Phantom Menace\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 1999, ademas,\n");	
+		printf("llego a recaudar 1027 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      }
+
+      break;
+
+      case 2: printf("\nStar Wars: Episode II – Attack of the Clones\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 2002, ademas,\n");	
+		printf("llego a recaudar 649 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      }
+
+      break;
+
+      case 3: printf("\nStar Wars: Episode III – Revenge of the Sith\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 2005, ademas,\n");	
+		printf("llego a recaudar 848 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      }
+
+      break;
+
+      case 4: printf("\nStar Wars: Episode IV A New Hope\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 1977, ademas,\n");	
+		printf("llego a recaudar 775,8 millones de dolares.\n");
+		regresarAlMenu();
+
+ 	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      }     
+      
+      break;
+
+      case 5: printf("\nStar Wars: Episode V - The Empire Strikes Back\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 1980, ademas,\n");	
+		printf("llego a recaudar 550,9 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();	
+	      }
+
+
+      break;
+
+      case 6: printf("\nStar Wars: Episode VI - Return of the Jedi\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 1983, ademas,\n");	
+		printf("llego a recaudar 475 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      } 
+      break;
+
+      case 7: printf("\nStar Wars: Episode VII – The Force Awakens\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 2015, ademas,\n");	
+		printf("llego a recaudar 2066 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      } 
+
+      break;
+
+      case 8: printf("\nStar Wars: Episode VIII - The Last Jedi\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 2017, ademas,\n");	
+		printf("llego a recaudar 1333 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      } 
+
+      break;
+
+      case 9: printf("\nStar Wars: Episode IX: The Rise of Skywalker\n");
+	      printf("\nDesea, usted, saber mas sobre la pelicula? [y/n] ");
+	      scanf(" %c", &decisionUsusario2);
+	      
+	      if (decisionUsusario2 == SI) {
+		printf("\nLa pelicula se estreno mundialmente en 2019, ademas,\n");	
+		printf("llego a recaudar 1074 millones de dolares.\n");
+		regresarAlMenu();
+
+	      } else if (decisionUsusario2 == NO) {
+		EXIT_SUCCESS;
+
+	      } else {
+		printf("Error: Usted ha ingresado una opcion invalida.");	
+		regresarAlMenu();
+	      } 
+
+      break;
+
+    }
+
+  } else {
+    printf("\nError: Usted ha ingresado una opcion invalida.\n");
+    regresarAlMenu();
   }
 
 }
