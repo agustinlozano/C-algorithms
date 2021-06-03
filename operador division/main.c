343015e94741a38050b899141a0908277bd3abae
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-06-02 21:23:27 -0300

    Agrega programa al repositorio Este programa muestra algunas caracteristicas del operador division.

---
+++ operador division/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+   printf("division entera:    5/4     es %6d\n", 5/4);
+   printf("division entera:    6/3     es %6d\n", 6/3);
+   printf("division entera:    7/4     es %6d\n", 7/4);
+   printf("division flotante:  7./4.   es %6.3f\n", 7./4.);
+   printf("division mixta:     7./4    es %6.3f\n", 7./4);
+
+   return 0;
+}
