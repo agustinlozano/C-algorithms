de161ae34cde756292472dca98c0b9e2b451776c
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-06-04 14:28:30 -0300

    Mejoras en el programa de fgets Corrige el programa y lo deja listo para utilizarlo en el desafio 8 de la practica de arreglos.

--- Arrays/fgets-function/main.c
+++ Arrays/fgets-function/main.c
@@ -3,19 +3,38 @@
 #define SIZE 50
 
 int main() {
-    char frase[SIZE];
+   char frase[SIZE];
+   int longDeFrase;
+   int contador = 0;
+   
+   printf("Introduzca una frase al programa: \n");
+   printf("Este programa admite 50 caracteres. Los espacios cuentan!\n");
+   fgets(frase, SIZE, stdin);
 
-    printf("Introduzca una frase al programa: \n");
-    fgets(frase, SIZE, stdin);
 
-    /* Mostrar arreglo por pantalla */
-    printf("\n{");
-    for (int l = 0; l<SIZE; l++) {
-      if (l == (SIZE-1)) {
-        printf("%c}", frase[l]);
-        break;
+      /* Averiguar la longitud */
+   for (int i = 0; i<SIZE; i++) {
+      char aux = frase[i];
+      if (aux == '\0') {
+	 contador = contador - 1;
+	 break; 
+      }
+      contador++;
+   }
+    
+
+      /* Mostrar arreglo por pantalla */
+   printf("\n\t'");
+   for (int l = 0; l<contador; l++) {
+      if (l == (contador-1)) {
+	 printf("%c'\n", frase[l]);
+	 break;
       }
       printf("%c", frase[l]);
-    }
-    return 0;
+   }
+
+    
+   printf("\nTu frase contiene %d caracteres\n", contador);
+
+   return 0;
 }
