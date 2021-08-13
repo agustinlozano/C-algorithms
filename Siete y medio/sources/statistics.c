e08765aa389f18c2cbc27c270ecc8b612533258f
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-08-13 19:12:34 -0300

    Mejora funcion para item adicional tres Ahora la funcion obtenerGananciaTotal maneja todos los casos posibles.

--- Siete y medio/sources/statistics.c
+++ Siete y medio/sources/statistics.c
@@ -31,23 +31,54 @@ void obtenerMayorApuesta(int mayorApuesta[]) {
 void obtenerGananciaTotal(int ganancias[], int tesoroBanca, const int numeroJugadores) {
     int gananciaTotalJugadores = 0, gananciaBanca = 0, diferenciaGanacia;
 
-    gananciaBanca = TESORO_INICIAL - tesoroBanca;
+    gananciaBanca = tesoroBanca - TESORO_INICIAL;
 
-    for (int i = 0; i<numeroJugadores; i++) {
+    if (gananciaBanca == 0) {
+        //La banca no gano ni perdio
+        for (int i = 0; i<numeroJugadores; i++) {
         gananciaTotalJugadores = gananciaTotalJugadores + ganancias[i];
-    }
+        }
 
-    diferenciaGanacia = gananciaBanca - gananciaTotalJugadores;
+        if (gananciaTotalJugadores == 0) {
+            printf("\nEn esta partida la banca no tuvo ganancias y los jugadores tampoco.\n\n");
+        }else if (gananciaTotalJugadores>0) {
+            printf("\nEl conjunto de jugadores logro mas ganancias que la banca.");
+            printf("\nY su ganancia total fue de $%d\n\n", gananciaTotalJugadores);
+        } else {
+            printf("\nEn esta partida la banca no tuvo ganancias y los jugadores perdidas.\n\n");
+        }
+    } else if (gananciaBanca>0) {
+        //la banca gano plata
+        for (int i = 0; i<numeroJugadores; i++) {
+            gananciaTotalJugadores = gananciaTotalJugadores + ganancias[i];
+        }
 
-    if (diferenciaGanacia == 0) {
-        printf("\nLa banca gano tanto dinero como el conjunto de jugadores,");
-        printf("\ny su ganancia total fue de $%d\n\n", gananciaBanca);
-    } else if (diferenciaGanacia>0) {
-        printf("\nLa banca gano mas dinero que el conjunto de jugadores,");
-        printf("\ny su ganancia total fue de $%d\n\n", gananciaBanca);
+        diferenciaGanacia = gananciaBanca - gananciaTotalJugadores;
+
+        if (diferenciaGanacia == 0) {
+            printf("\nLa banca gano tanto dinero como el conjunto de jugadores,");
+            printf("\ny su ganancia total fue de $%d\n\n", gananciaBanca);
+        } else if (diferenciaGanacia>0) {
+            printf("\nLa banca gano mas dinero que el conjunto de jugadores,");
+            printf("\ny su ganancia total fue de $%d\n\n", gananciaBanca);
+        } else {
+            printf("\nEl conjunto de jugadores logro mas ganancias que la banca.");
+            printf("\nY su ganancia total fue de $%d\n\n", gananciaTotalJugadores);
+        }
     } else {
-        printf("\nEl conjunto de jugadores logro mas ganancias que la banca.");
-        printf("\nY su ganancia total fue de $%d\n\n", gananciaTotalJugadores);
+        //La banca perdio plata
+        for (int i = 0; i<numeroJugadores; i++) {
+            gananciaTotalJugadores = gananciaTotalJugadores + ganancias[i];
+        }
+
+        if (gananciaTotalJugadores == 0) {
+            printf("\nEn esta partida la banca tuvo perdidas y los jugadores no ganaron nada.\n\n");
+        }else if (gananciaTotalJugadores>0) {
+            printf("\nEl conjunto de jugadores logro mas ganancias que la banca.");
+            printf("\nY su ganancia total fue de $%d\n\n", gananciaTotalJugadores);
+        } else {
+            printf("\nEn esta partida la banca tuvo perdidas y los jugadores tambien.\n\n");
+        }
     }
 }
 
